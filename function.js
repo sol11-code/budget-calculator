34f354a2d8ec827b09ed5e1b2e96523d4862a0a9
sol11-code <143833266+sol11-code@users.noreply.github.com>
2024-03-11 08:15:48 -0700

    added files

---
+++ function.js
@@ -0,0 +1,17 @@
+let utils = {};
+
+utils.get = ('https://api-demo.cartwebapp.com/data/2024') => {
+    
+}
+
+let tax = document.getElementById(tax)
+
+let gross= document.getElementById(gross-income)
+
+let monthly = document.getElementById(monthly)
+
+let option = document.getElementById('option')
+
+addEventListener('clicked') => { 
+
+}
